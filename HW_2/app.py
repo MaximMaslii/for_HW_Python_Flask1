e449d6fa94fa0d1dc5d79faa0f281691269dd589
MaximMaslii <140587592+MaximMaslii@users.noreply.github.com>
2023-12-26 15:02:58 +0200

    Updated HW_2

--- HW_2/app.py
+++ HW_2/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Замените 'your_secret_key' на случайную строку
+app.secret_key = 'JohnDoe' 
 
 @app.route('/')
 def index():
@@ -13,7 +13,6 @@ def welcome():
         username = request.form['username']
         email = request.form['email']
 
-        # Создаем cookie с данными пользователя
         response = make_response(render_template('welcome.html', username=username))
         response.set_cookie('username', username)
         response.set_cookie('email', email)
@@ -22,7 +21,7 @@ def welcome():
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    # Удаляем cookie с данными пользователя
+
     response = make_response(redirect(url_for('index')))
     response.delete_cookie('username')
     response.delete_cookie('email')
